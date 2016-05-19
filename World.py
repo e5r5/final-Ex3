93073bc1150f04b6efa03efccc01cb34f8bce711
shaharelbaz <shahar_112@walla.com>
2016-05-19 20:02:10 +0300

    Update World.py

--- World.py
+++ World.py
@@ -29,6 +29,9 @@ button1_window = canvas.create_window(10, 10, window=button1)
 
 
 class Robot:
+    x=0
+    y=0
+    id=0
     def __init__(self,id,x,y):
         start_time=time.time()
         self.id=id
@@ -41,13 +44,11 @@ class Robot:
         canvas.create_oval(x - 5, y - 5, x + 5, y + 5, width=1, fill='red')
 
 
-
     def WriteMSGtoFile(s):
         with open('MSG_history'+id+'.txt','a') as file:
             file.write(s+'\n')
     def addPath(self,s):
         self.history_path[self.history_path.size]=s
-
 
 class MSG:
     def __init__(self, IDmsg, timeMsg, senderID, dataMSG, power, sourceMsg):
@@ -69,17 +70,29 @@ class MSG:
 
 
 class Simulator:
-
+    robots = []
     def __init__(self):
         start_time=time.time()
-        self.robots=[]
         for i in range(0,100):
             x=random.random()*1000
             y=random.random()*750
-            while ((x>=100 and x<=200 and y>=100 and y<=200)or (x>=300 and x<=700 and y>=600 and y<=700)):
+            while ((x>=95 and x<=205 and y>=95 and y<=205)or (x>=295 and x<=705 and y>=595 and y<=705)):
                 x = random.random() * 1000
                 y = random.random() * 750
             self.robots.insert(i,Robot(i,x,y))
+        self.robots.insert(0,Robot(0,230,110))
+
+    def MoveRobot( self,i, x, y):
+        r = Simulator.robots.pop(i)
+        if (r.x >= 330 and r.x <= 600 and r.y >= 330 and r.y <= 500):
+            canvas.create_oval(r.x - 5, r.y - 5, r.x + 5, r.y + 5, width=0, fill='gray')
+        else:
+            canvas.create_oval(r.x - 5, r.y - 5, r.x + 5, r.y + 5, width=0, fill='white')
+
+        canvas.create_oval(x - 5, y - 5, x + 5, y + 5, width=1, fill='red')
+        Simulator.robots.insert(i, Robot(i, x, y))
+
+
 
 
 def swap(a, b, arr):
@@ -91,9 +104,8 @@ def RandomArr(arr):
          for i in range(0, 100):
              swap(i, random.randint(0, 100), arr)
 
+s = Simulator()
 
-
-Simulator()
 
 canvas.bind('<Button-1>',  onMiddleClick)
 canvas.pack(expand=YES, fill=BOTH)
