fe798a084bc52aa8e8b827c6dbd4899db2c4c5a5
erez yehezkel <Erez5749520@gmail.com>
2016-05-20 17:17:12 +0300

    Update World.py

--- World.py
+++ World.py
@@ -1,6 +1,8 @@
 from Tkinter import *
 import random
 import time
+
+########################################GUI######################################
 def showPosEvent(event):
     print ' X=%s Y=%s' % ( event.x, event.y)
 
@@ -14,8 +16,6 @@ tkroot = Tk()
 #labelfont = ('courier', 10, 'bold')
 
 canvas = Canvas(width=1000, height=750, bg='white')
-
-
 canvas.create_rectangle(100, 100, 200, 200, width=1, fill='black')
 #canvas.create_rectangle(100, 400,550 , 500, width=5, fill='gray')
 canvas.create_rectangle(300, 600,700 , 700, width=3, fill='black')
@@ -27,22 +27,34 @@ button1_window = canvas.create_window(10, 10, window=button1)
 #button2.configure(width = 10)
 #button2_window = canvas.create_window(10, 10, window=button2)
 
-
+###########################################################################################
 class Robot:
-    x=0
-    y=0
-    id=0
-    def __init__(self,id,x,y):
-        start_time=time.time()
-        self.id=id
-        self.x=x
-        self.y=y
-        start_time=time.time()
-        self.history_path=[]
-        self.env=[]
+    x = 0
+    y = 0
+    id = 0
+    battery = 70
+    IsWhite = None
 
+    def __init__(self, id, x, y):
+        start_time = time.time()
+        self.id = id
+        self.x = x
+        self.y = y
+        start_time = time.time()
+        self.history_path = []
+        self.env = []
         canvas.create_oval(x - 5, y - 5, x + 5, y + 5, width=1, fill='red')
 
+        if ((x >= 330 and x <= 600 and y >= 330 and y <= 500)):
+            self.IsWhite = False
+        else:
+            self.IsWhite = True
+
+    def control_battery(self):
+        if (self.IsWhite and self.battery < 100):
+            self.battery + 1
+        elif (not (self.IsWhite and self.battery > 0)):
+            self.battery - 1
 
     def WriteMSGtoFile(s):
         with open('MSG_history'+id+'.txt','a') as file:
@@ -50,6 +62,7 @@ class Robot:
     def addPath(self,s):
         self.history_path[self.history_path.size]=s
 
+############################################################################################
 class MSG:
     def __init__(self, IDmsg, timeMsg, senderID, dataMSG, power, sourceMsg):
         start_time=time.time()
@@ -69,30 +82,7 @@ class MSG:
         self.counterMSG=self.counterMSG+1
 
 
-class Simulator:
-    robots = []
-    def __init__(self):
-        start_time=time.time()
-        for i in range(0,100):
-            x=random.random()*1000
-            y=random.random()*750
-            while ((x>=95 and x<=205 and y>=95 and y<=205)or (x>=295 and x<=705 and y>=595 and y<=705)):
-                x = random.random() * 1000
-                y = random.random() * 750
-            self.robots.insert(i,Robot(i,x,y))
-        self.robots.insert(0,Robot(0,230,110))
-
-    def MoveRobot( self,i, x, y):
-        r = Simulator.robots.pop(i)
-        if (r.x >= 330 and r.x <= 600 and r.y >= 330 and r.y <= 500):
-            canvas.create_oval(r.x - 5, r.y - 5, r.x + 5, r.y + 5, width=0, fill='gray')
-        else:
-            canvas.create_oval(r.x - 5, r.y - 5, r.x + 5, r.y + 5, width=0, fill='white')
-
-        canvas.create_oval(x - 5, y - 5, x + 5, y + 5, width=1, fill='red')
-        Simulator.robots.insert(i, Robot(i, x, y))
-
-
+############################## class Simulator:################################################
 
 
 def swap(a, b, arr):
@@ -104,7 +94,15 @@ def RandomArr(arr):
          for i in range(0, 100):
              swap(i, random.randint(0, 100), arr)
 
-s = Simulator()
+robots = []
+for i in range(0,100):
+    x=random.random()*1000
+    y=random.random()*750
+    while ((x>=95 and x<=205 and y>=95 and y<=205)or (x>=295 and x<=705 and y>=595 and y<=705)):
+            x = random.random() * 1000
+            y = random.random() * 750
+    robots.insert(i,Robot(i,x,y))
+
 
 
 canvas.bind('<Button-1>',  onMiddleClick)
