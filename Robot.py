edee99c03b9b7cd020b655116485dee46ba8c6b3
shiraOzari <e5r5@walla.com>
2016-05-29 21:14:32 +0300

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -51,6 +51,7 @@ class Robot:
         else:
             self.IsWhite = True
 
+
         #built for tree matrix for distance to another robots
         if(isTree):
             x1= int(x) +1
@@ -68,36 +69,35 @@ class Robot:
                                             j >= 300 + 1 and j <= 700 + 1 and i >= 600 + 1 and i <= 700 + 1)):
                         self.disForTree[i][j] = -5
             self.disForTree[y1][x1] = 0
-            arr = [x1, y1]
+            arr = [y1, x1]
             start = 0
             end = 1
             # calculation the distance
             while (start < end):
 
-                x1 = arr[start]
-                y1 = arr[start + 1]
+                y1 = arr[start]
+                x1 = arr[start + 1]
                 start = start + 2
-                if (self.disForTree[y1][x1 + 1] == -1):
-                    self.disForTree[y1][x1 + 1] = self.disForTree[y1][x1] + 1
-                    arr.append(x1 + 1)
-                    arr.append(y1)
-                    end = end + 2
-                if (self.disForTree[y1][x1 - 1] == -1):
-                    self.disForTree[y1][x1 - 1] = self.disForTree[y1][x1] + 1
-                    arr.append(x1 - 1)
-                    arr.append(y1)
-                    end = end + 2
-                if (self.disForTree[y1][x1 + 1] == -1):
-                    self.disForTree[y1][x1 + 1] = self.disForTree[y1][x1] + 1
-                    arr.append(x1)
+                if (self.disForTree[y1 + 1][x1] == -1):
+                    self.disForTree[y1 + 1][x1] = self.disForTree[y1][x1] + 1
                     arr.append(y1 + 1)
+                    arr.append(x1)
+                    end = end + 2
+                if (self.disForTree[y1 - 1][x1] == -1):
+                    self.disForTree[y1 - 1][x1] = self.disForTree[y1][x1] + 1
+                    arr.append(y1 - 1)
+                    arr.append(x1)
+                    end = end + 2
+                if (self.disForTree[y1][x1 + 1] == -1):
+                    self.disForTree[y1][x1 + 1] = self.disForTree[y1][x1] + 1
+                    arr.append(y1)
+                    arr.append(x1 + 1)
                     end = end + 2
                 if (self.disForTree[y1][x1 - 1] == -1):
                     self.disForTree[y1][x1 - 1] = self.disForTree[y1][x1] + 1
-                    arr.append(x1)
-                    arr.append(y1 - 1)
+                    arr.append(y1)
+                    arr.append(x1 - 1)
                     end = end + 2
-
 
 
 
